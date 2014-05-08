eae2352783c6f7c8f5ef1a1e197f87a2881a1a9a
Kevin Ndung'u <kevgathuku@gmail.com>
2014-05-08 13:47:21 +0300

    Change root url method. Add sanitization

--- hello.py
+++ hello.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-from til import factsChecker
+from til import factsChecker, sanitizeFacts
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route('/facts')
 def facts(posts=None):
-	return render_template('facts.html', posts=factsChecker())
+	return render_template('facts.html', posts=sanitizeFacts())
 
 if __name__ == '__main__':
     app.run(debug=True)
